d4b38fda6ee9ff0f283b64418a3f517e0671722d
Julio Cesar <juliusceshar@gmail.com>
2016-08-17 01:18:41 -0400

    init2

--- protected/config/web.php
+++ protected/config/web.php
@@ -1,5 +1,20 @@
 <?php
 
 return [
+    'bootstrap' => ['debug'],
+    'modules' => [
+        // ...
+
+        'debug' => [
+            'class' => 'yii\debug\Module',
+            'allowedIPs' => ['127.0.0.1', '::1'],
+        ],
+        'gii' => [
+            'class' => 'yii\gii\Module',
+            'allowedIPs' => ['127.0.0.1', '::1'],
+        ],
+
+        // ...
+    ]
 ];
 
